b62c31e7fedd4a4d7748e189b70c25ed8f38493f
YOUSEF ALSARRAJ <yousef.edu@outlook.com>
2022-09-16 14:51:13 +0300

    fixed products

--- src/components/yousef_compoments/Products.js
+++ src/components/yousef_compoments/Products.js
@@ -13,26 +13,32 @@ const Products = () => {
     <p>these products are full-stack products and were built completly by our teams </p>
   </div>
   <div className="row1-container" >
-    <Link to="/ISIL" className="box box-down cyan" >
+    <div className="box box-down cyan">
+    <Link style={  {textDecoration: "none"}} to="/ISIL"  >
       <h2>Işın Daylight</h2>
       <p>Monitors activity to identify project roadblocks</p>
       <img  src="assets/img/isin_daylight.png" alt="logo_isin" className=" img_product  " /> 
     </Link>
-
-    <Link to="faturahub" className="box red " >
+    </div>
+  
+     <div className="box red ">
+     <Link style={  {textDecoration: "none"}} to="faturahub"  >
       <h2>FaturaHub</h2>
       <p>Scans our talent network to create the optimal team for your project</p>
       <img  src="assets/img/Fatura_hub.png" alt="logo_faturahub" className="img_product " /> 
     </Link>
+     </div>
 
-    <Link to="element" className="box box-down blue" >
+    <div className="box box-down blue">
+    <Link style={  {textDecoration: "none"}} to="element"  >
       <h2>Elements</h2>
       <p>Uses data from past projects to provide better delivery estimates</p>
       <img  src="assets/img/ERP.png" alt="ERP" className="img_product element_img"/>
     </Link>
+    </div>
+
   </div>   
-      </div>
-      
+  </div>
     </section>
   );
 };
