ebc98a5c93ce3b0cd4fe41bbcf40066d78ceb25a
YOUSEF ALSARRAJ <yousef.edu@outlook.com>
2022-10-14 10:34:23 +0300

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import PageHeader from '../components/common/PageHeader';
-import PageMeta from '../components/common/PageMeta';
-import ContactBox from '../components/contact/ContactBox';
 import ContactFormTwo from '../components/contact/ContactFormTwo';
 import FooterOne from '../layout/Footer/FooterOne';
 import Navbar from '../layout/Header/Navbar';
@@ -13,7 +11,8 @@ const Contact = () => {
       <Navbar />
       <PageHeader
         title='Contact Us'
-        desc='Seamlessly actualize client-based users after out-of-the-box value data through frictionless expertise. Proactively coordinate quality quality vectors vis-a-vis supply chains. Quickly engage client-centric web services.'
+        desc='Seamlessly actualize client-based users after out-of-the-box value data through frictionless expertise. 
+        Proactively coordinate quality quality vectors vis-a-vis supply chains. Quickly engage client-centric web services.'
       />
        <ContactFormTwo />
      
