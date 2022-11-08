bd648ce365af3441c99f0dfcc5ab2d0a6d5affdb
YOUSEF ALSARRAJ <yousef.edu@outlook.com>
2022-11-08 20:25:05 +0300

    Added CAPTCHA and fixed form

--- src/components/contact/ContactFormTwo.js
+++ src/components/contact/ContactFormTwo.js
@@ -1,81 +1,27 @@
-import React , {useState}from 'react';
-import { Form  } from 'react-bootstrap';
+import React , {useRef , useState ,useCallback  }from 'react';
+import * as yup from 'yup';
+import ReCAPTCHA from "react-google-recaptcha"
+import { Formik , Field ,Form ,ErrorMessage } from "formik"
 
-const ContactFormTwo = () => { 
- const [FullName, setFullName] = useState("") ;
- const[CompanyName , setCompanyName] = useState("") ; 
- const [ContactNumber , setContactNumber] = useState("") ; 
- const[email , setemail] = useState("") ; 
- const [body , setbody] = useState("") ; 
- const [validated, setValidated] = useState(false);
- 
- const data = {
-  Parameter:{
-    Target:"yousef.edu@outlook.com",
-    NotificationProfileId :"c49aec07-0243-433a-b03a-1ca3ba276c68",
-    NotificationTemplateId:"100a30f9-2e4f-4718-b038-7dceb7d0c278",
-    Subject:"Contact form",
-    Parameters :[{
-      Name:"Name",
-      Value: FullName
-    },{
-      Name:"Company",
-      Value: CompanyName
-    },{
-      Name:"Email",
-      Value: email
-    },{
-      Name:"Phone",
-      Value: ContactNumber
-    },{
-      Name:"Message",
-      Value: body
-    }]
-  }
-}
-const checkData = () =>{
-  if (FullName ===""){return true}
-  if (CompanyName ===""){return true}
-  if (ContactNumber ===""){return true}
-  if (email ===""){return true}
-  if (body ===""){return true}
-}
-const checkFilled = () => {
-  if (FullName !=="" && CompanyName !=="" && ContactNumber !=="" && email !=="" && body !=="" ){   
-  setFullName("") ; 
-  setCompanyName("") ;
-  setContactNumber("") ; 
-  setemail("") ; 
-  setbody("") ;
-  return true; 
+const ContactFormTwo = () => {
+  const [disableSubmit,setDisableSubmit] = useState(true);
+  const captchaRef = useRef(null) ;
+
+const yes_dome = () =>{
+  setDisableSubmit(false);
 }
 
-}
+const validatioSchemas = yup.object().shape({
+  Name: yup.string().required("Required"),
+  Company: yup.string().required("Required"),
+  Email: yup.string().email("Invalid email address").required("Required"),
+  Phone: yup.string().required("Required"),
+  Message: yup.string().required("Required"),
 
- const handleSubmit = (event) => {
-  const form = event.currentTarget;
-  if (checkData() ) {
-    event.preventDefault();
-    event.stopPropagation();
-    setValidated(false);
-  }
-
-  setValidated(true);
-  if(validated){
-   const requestOptions = {
-     method: 'POST',
-     headers: { 'Content-Type': 'application/json' },
-     body: JSON.stringify(data)
- };
- 
-   fetch('https://gateway.elementserp.com/notification/Notification/send', requestOptions)
-   .then(response => response.json())
-  } 
-  checkFilled(); 
-
-}
+});
   return (
     <>
+ 
       <section
         className='contact-us-form pt-60 pb-120'
         style={{
@@ -83,6 +29,7 @@ const checkFilled = () => {
             "url('assets/img/shape/contact-us-bg.svg')no-repeat center bottom",
         }}
       >
+             
         <div className='container'>
           <div className='row justify-content-lg-between align-items-center'>
             <div className='col-lg-6 col-md-8'>
@@ -92,107 +39,125 @@ const checkFilled = () => {
                 You are welcome to discuss your project or your needs with us. We can collaborate with you to put the best possible solutions into action.
                 </p>
               </div>
-               <Form noValidate validated={validated}  className='register-form'>
-                <div className='row'>
-                  <div className='col-sm-6'>
-                    <label htmlFor='FullName' className='mb-1'>
-                      Full Name <span className='text-danger'>*</span>
-                    </label>
-                    <div className='Form.Control-group mb-3'>
-                      <Form.Control
-                        value={FullName}
-                        required
-                        type="text"
-                        className='form-control'
-                        placeholder="Full Name"
-                        defaultValue="Defualt"
-                        onChange={(e) => setFullName(e.target.value)}
-                      />
-                       <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </div>
-                  </div>
-                  <div className='col-sm-6 '>
-                    <label htmlFor='CompanyName' className='mb-1'>
-                      Company Name <span className='text-danger'>*</span>
-                    </label>
-                    <div className='Form.Control-group mb-3'>
-                      <Form.Control
-                        required
-                        name="CompanyName"
-                        value={CompanyName}
-                        type='text'
-                        className='form-control'
-                        id='CompanyName'
-                        placeholder='Company Name'
-                        aria-label='Company Name'
-                        onChange={(e) => setCompanyName(e.target.value)}
-                      />
-                       <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                     
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <label htmlFor='Contact Number' className='mb-1'>
-                    Contact Number <span className='text-danger'>*</span>
-                    </label>
-                    <div className='Form.Control-group mb-3'>
-                      <Form.Control
-                        required
-                        name="ContactNumber"
-                        value={ContactNumber}
-                        type='text'
-                        className='form-control'
-                        id='Contact Number'
-                      
-                        placeholder='Contact Number'
-                        aria-label='Contact Number'
-                        onChange={(e) => setContactNumber(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                  <div className='col-sm-6'>
-                    <label htmlFor='email' className='mb-1'>
-                      Email<span className='text-danger'>*</span>
-                    </label>
-                    <div className='Form.Control-group mb-3'>
-                      <Form.Control
-                        required
-                        name="email"
-                        value={email}
-                        type='email'
-                        className='form-control'
-                        id='email'
-                        
-                        placeholder='Email'
-                        aria-label='Email'
-                        onChange={(e) => setemail(e.target.value)}
-                      />
-                    </div>
-                  </div>
-                  <div className='col-12'>
-                    <label htmlFor='yourMessage' className='mb-1'>
-                      Message <span className='text-danger'>*</span>
-                    </label>
-                    <div className='Form.Control-group mb-3'>
-                      <textarea
-                         required
-                         name="body"
-                        value={body}
-                        className='form-control'
-                        id='yourMessage'
-                        
-                        placeholder='
-                        Can you inform us about your project or your needs?'
-                        style={{ height: '120px' }}
-                        onChange={(e) => setbody(e.target.value)}
-                      ></textarea>
-                    </div>
-                  </div>
-                </div>
-                <button type='button' className='btn btn-primary mt-4' onClick={handleSubmit} >
-                Send
-                </button>
-              </Form> 
+              <Formik initialValues = {{
+                                        Name:"",
+                                        Company:"",
+                                        Email:"",
+                                        Phone:"",
+                                        Message:"",
+                                      }}
+                        validationSchema={validatioSchemas}
+                        onSubmit={(values , { resetForm }) => {
+                          const data = {
+                              Parameter:{
+                                Target:"yousef.edu@outlook.com",
+                                NotificationProfileId :"c49aec07-0243-433a-b03a-1ca3ba276c68",
+                                NotificationTemplateId:"100a30f9-2e4f-4718-b038-7dceb7d0c278",
+                                Subject:"Contact form",
+                                Parameters :[{
+                                  Name:"Name",
+                                  Value: values.Name
+                                },{
+                                  Name:"Company",
+                                  Value: values.Company
+                                },{
+                                  Name:"Email",
+                                  Value: values.Email
+                                },{
+                                  Name:"Phone",
+                                  Value: values.Phone
+                                },{
+                                  Name:"Message",
+                                  Value: values.Message
+                                }]
+                              }
+                            }
+                            const requestOptions = {
+                                   method: 'POST',
+                                   headers: { 'Content-Type': 'application/json' },
+                                   body: JSON.stringify(data)
+                               };
+                               const token = captchaRef.current.getValue();
+                            fetch('https://gateway.elementserp.com/notification/Notification/send', requestOptions)
+                                    .then(response => response.json())
+                                    console.log(JSON.stringify(data)) ;
+                                     resetForm();
+                                    captchaRef.current.reset();
+                                    setDisableSubmit(true);
+                           
+                          }} >
+                       {({  errors, touched}) =>(
+                        <Form  className='register-form'>
+                             <div className='row'>
+                                <div className='col-sm-6'>
+                                  <label htmlFor='FullName' className='mb-1'>
+                                     Full Name <span className='text-danger'>*</span>
+                                  </label>
+                                      <div className='Form.Control-group mb-3'>
+                                      <Field name="Name" className='form-control'/>
+                                      {errors.Name && touched.Name ? (
+                                        <div>{errors.Name}</div>
+                                      ) : null}
+                                      </div>
+                                  </div>
+
+                                  <div className='col-sm-6 '>
+                                    <label htmlFor='CompanyName' className='mb-1'>
+                                       Company Name <span className='text-danger'>*</span>
+                                    </label>
+                                    <div className='Form.Control-group mb-3'>
+                                      <Field name="Company" className='form-control'/>
+                                      {errors.Company && touched.Company ? (
+                                        <div>{errors.Company}</div>
+                                      ) : null}
+                                      </div>
+                                  </div>
+                                  <div className='col-sm-6'>
+                                    <label htmlFor='Contact Number' className='mb-1'>
+                                       Contact Number <span className='text-danger'>*</span>
+                                    </label>
+                                    <div className='Form.Control-group mb-3'>
+                                            <Field name="Phone" className='form-control' />
+                                            {errors.Phone && touched.Phone ? (
+                                              <div>{errors.Company}</div>
+                                            ) : null}
+                                          </div>
+                                    </div>
+                                      <div className='col-sm-6'>
+                                            <label htmlFor='email' className='mb-1'>
+                                              Email<span className='text-danger'>*</span>
+                                            </label>
+                                            <div className='Form.Control-group mb-3'>
+                                            <Field name="Email" className='form-control' type="email" />
+                                            {errors.Email && touched.Email ? <div>{errors.Email}</div> : null}
+                                          </div>
+                                      
+                                      </div>
+                                    <div className='col-12'>
+                                      <label htmlFor='yourMessage' className='mb-1'>
+                                        Message <span className='text-danger'>*</span>
+                                      </label>
+                                      <div className='Form.Control-group mb-3'>
+                                      <Field component="textarea"   style={{ height: '20vh' }} name="Message" className='form-control'/>
+                                      {errors.Message && touched.Message ? (
+                                              <div>{errors.Message}</div>
+                                            ) : null}
+                                            
+                                      </div>
+                                    </div>
+                                </div>
+                                <ReCAPTCHA
+                                   sitekey="6LeqNO0iAAAAADA-hAtgmrbHQn6aK3XWhV2UPu4_"
+                                   ref={captchaRef}
+                                   onChange={yes_dome}
+                                 />
+                                      <button type="submit " className='btn btn-primary mt-4'  disabled={disableSubmit}>Submit</button>
+                                
+                        </Form>
+                       )}
+
+              </Formik>
+                                 
             </div>
             <div className='col-lg-5 col-md-10'>
               <div className='contact-us-img'>
@@ -205,7 +170,7 @@ const checkFilled = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> 
     </>
   );
 };
